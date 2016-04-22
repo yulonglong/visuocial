c5df227f62dee659dd03ccf56bbd91d11f28d3f4
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2016-04-22 12:09:42 +0800

    Added auth and database sample

---
+++ config/auth_sample.js
@@ -0,0 +1,38 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'        : 'xxxxxx', // your App ID
+        'clientSecret'    : 'xxxxxx', // your App Secret
+        'callbackURL'     : 'http://localhost:1337/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'        : 'xxxxx',
+        'consumerSecret'     : 'xxxxx',
+        'callbackURL'        : 'http://localhost:1337/auth/twitter/callback'
+    },
+
+    'instagramAuth' : {
+        'clientID'         : 'xxxxx',
+        'clientSecret'     : 'xxxxx',
+        'callbackURL'      : 'http://localhost:1337/auth/instagram/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'         : 'xxxxxx',
+        'clientSecret'     : 'xxxxxx',
+        'callbackURL'      : 'http://localhost:1337/auth/google/callback'
+    },
+
+    'mashape': {
+        'key'  : 'xxxxx' // Mashape Market/API key
+    },
+    
+    'sendgrid': {
+        'key'  : 'xxxxx' // SendGrid token/key
+    }
+
+};
